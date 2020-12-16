99f4b84a6560609cf1c608e9e1532e0e246d1d67
Ori Weinstock <ori.weinstock@gmail.com>
2020-12-16 15:36:53 +0200

    Basic CSS layout, Location service psuedo-functions

---
+++ js/services/storage-service.js
@@ -0,0 +1,15 @@
+export const storageService = {
+    saveToStorage,
+    loadFromStorage
+};
+
+function loadFromStorage(key) {
+    var json = localStorage.getItem(key)
+    var value = JSON.parse(json)
+    return value;
+}
+
+function saveToStorage(key, value) {
+    var json = JSON.stringify(value);
+    localStorage.setItem(key, json)
+}
